33b2944e6fb64ea15c930fab4d0ab118bf801020
CodeElite1337 <>
2023-09-05 21:28:34 +0000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,58 @@
 /* script.js */
-
 /**
- * Declare constants for DOM elements and possible choices
- */
 
-const buttons = document.getElementsByClassName("control");
+* Declare constants for DOM elements 
+* and possible choices
+*/
+
+const buttons = document.getElementsByClasName("control");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const choices = ["rock", "paper", "scissors"];
+const choices = ["rock" , "paper" , "scissors"];
+
 
 /**
- * Add event listener to all the buttons
- */
+
+* Add event listener to all the buttons 
+*/
 
 for (let button of buttons) {
-    button.addEventListener("click", function () {
-        let playerChoice = this.getAttribute("data-choice");
-        playGame(playerChoice);
-    });
+button.addEventListener ("click", function () { 
+let playerChoice = this.getAttribute("data-choice");
+playGame(playerChoice);
+});
 }
 
+
 /**
- * The main game function. Accepts one parameter, which
- * is the data-choice value of the selected button
- */
+* The main game function. Accepts one parameter, which
+* is the data-choice value of the selected button 
+*/
 
-function playGame(playerChoice) {
-    // Generate a random computer choice (0, 1, or 2)
-    let computerChoice = Math.floor(Math.random() * 3);
+function playGame(playChoice) {
 
-    // Update the player and computer images
-    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
-    playerImage.alt = choices[playerChoice];
-    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
-    computerImage.alt = choices[computerChoice];
+playerImage.src = 'assets/images/${choices[playerChoice]}.png';
+playerImage.alt = choices[playerChoice];
 
-    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
-    updateScores(result);
+let computerChoice = Math.floor() * 3);
+
+computerImage.src = ' assets/images/${choices [computerChoice]}.png';
+computerImage.alt = choices [computerChoice];
+
+let result = checkWinner(choices [computerChoice], choices [playerChoice]);
+
+updateScores(result);
+
 }
 
+
 /**
- * Checks to see who the winner is, it accepts two strings as parameters
- */
+* Checks to see who the winner is, it accepts two strings as 
+* 
+*/
 function checkWinner(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) {
         return "draw";
