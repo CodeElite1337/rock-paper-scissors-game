111b0f86cac78bbbbb4956a593cc991f61695bd9
CodeElite1337 <>
2023-09-06 03:06:20 +0000

    all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,58 +1,49 @@
-/* script.js */
 /**
-
-* Declare constants for DOM elements 
-* and possible choices
-*/
-
-const buttons = document.getElementsByClasName("control");
+ * Declare constants for DOM elements
+ * and possible choices
+ */
+const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
 const playerImage = document.getElementById("player-image");
 const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
-const choices = ["rock" , "paper" , "scissors"];
-
+const choices = ["rock", "paper", "scissors"];
 
 /**
-
-* Add event listener to all the buttons 
-*/
-
+ * Add event listener to all the buttons
+ */
 for (let button of buttons) {
-button.addEventListener ("click", function () { 
-let playerChoice = this.getAttribute("data-choice");
-playGame(playerChoice);
-});
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    });
 }
 
-
 /**
-* The main game function. Accepts one parameter, which
-* is the data-choice value of the selected button 
-*/
+ * The main game function. Accepts one parameter, which
+ * is the data-choice value of the selected button
+ */
+function playGame(playerChoice) {
+    playerImage.src = `assets/images/${choices[playerChoice]}.png`;
+    playerImage.alt = choices[playerChoice];
 
-function playGame(playChoice) {
+    let computerChoice = Math.floor(Math.random() * 3);
 
-playerImage.src = 'assets/images/${choices[playerChoice]}.png';
-playerImage.alt = choices[playerChoice];
+    computerImage.src = `assets/images/${choices[computerChoice]}.png`;
+    computerImage.alt = choices[computerChoice];
 
-let computerChoice = Math.floor() * 3);
+    console.log(`Player choice: ${choices[playerChoice]}`);
+    console.log(`Player image source: ${playerImage.src}`);
 
-computerImage.src = ' assets/images/${choices [computerChoice]}.png';
-computerImage.alt = choices [computerChoice];
+    let result = checkWinner(choices[computerChoice], choices[playerChoice]);
 
-let result = checkWinner(choices [computerChoice], choices [playerChoice]);
-
-updateScores(result);
-
+    updateScores(result);
 }
 
-
 /**
-* Checks to see who the winner is, it accepts two strings as 
-* 
-*/
+ * Checks to see who the winner is, it accepts two strings as
+ */
 function checkWinner(computerChoice, playerChoice) {
     if (computerChoice === playerChoice) {
         return "draw";
